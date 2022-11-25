05cc84ce3618ae659261c72b6d305fe45c1571fe
scarletalfaro <scarletalfaro9@gmail.com>
2022-11-25 13:35:58 -0300

    mi trabajo de pokemon

---
+++ src/Boton.js
@@ -0,0 +1,9 @@
+function Boton() {
+    return (
+        <>
+        <input type="text"></input>
+        <button type="button" class="btn btn-success">Buscar</button>
+        </>
+    )
+}
+export default Boton;
