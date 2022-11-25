fd9a760dc96ed6087e6602d9a1ae1f6fdf240fdc
scarletalfaro <scarletalfaro9@gmail.com>
2022-11-25 15:33:57 -0300

    Añade tabla

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     }
     const onclickfn = () => {
         //setPokemon( { label:pokemon})
-        console.log(pokemon, "https://pokeapi.co/api/v2/pokemon/" + pokemon )
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon )
+        console.log(pokemon, "https://pokeapi.co/api/v2/pokemon/" + pokemon)
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
             .then(response => response.json())
             .then(
                 (pokemonData) => {
@@ -28,10 +28,34 @@ function App() {
                 <input type="text" value={pokemon} onChange={onchangefn} />
                 <button type="" className="btn btn-light" onClick={onclickfn}>buscar</button>
 
-                <img src={pokemonD.sprites.front_default}></img>
-                <img src={pokemonD.sprites.back_default}></img>
+                <img src={pokemonD.sprites.front_default} className="imagen1"></img>
+                <img src={pokemonD.sprites.back_default} className="imagen2"></img>
 
-                <h2>{pokemonD.name}</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">orden de tu pokemon</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">experiencia</th>
+                            <th scope="col">identificador</th>
+                            <th scope="col">altura</th>
+                            
+
+
+
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">{pokemonD.order}</th>
+                            <td>{pokemonD.name}</td>
+                            <td>{pokemonD.base_experience}</td>
+                            <td>{pokemonD.id}</td>
+                            <td>{pokemonD.height}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     );
